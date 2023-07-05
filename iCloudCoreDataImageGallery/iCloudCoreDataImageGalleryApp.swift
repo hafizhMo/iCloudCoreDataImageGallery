1bbb7ff17ee303ad1912dfca4e5941923c747c78
hafizhMo <hafizh.hizb@gmail.com>
2023-07-05 21:39:51 +0700

    Initial Commit

---
+++ iCloudCoreDataImageGallery/iCloudCoreDataImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCloudCoreDataImageGalleryApp.swift
+//  iCloudCoreDataImageGallery
+//
+//  Created by Hafizh Mo on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct iCloudCoreDataImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
