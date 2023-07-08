a65a76f1a1ad9e3a3e60820a6ea8d6bdeb2d7e09
hafizhMo <hafizh.hizb@gmail.com>
2023-07-08 15:44:53 +0700

    zip and decode json

--- iCloudCoreDataImageGallery/ContentView.swift
+++ iCloudCoreDataImageGallery/ContentView.swift
@@ -9,10 +9,13 @@ import SwiftUI
 import PhotosUI
 
 struct ContentView: View {
+  @EnvironmentObject var shareService: ShareService
+  @Environment(\.managedObjectContext) var moc
   @FetchRequest(sortDescriptors: [SortDescriptor(\.name)])
   private var myImages: FetchedResults<MyImage>
   @StateObject private var imagePicker = ImagePicker()
   @State private var formType: FormType?
+  @State private var imageExists = false
   let columns = [GridItem(.adaptive(minimum: 100))]
 
   var body: some View {
@@ -56,8 +59,48 @@ struct ContentView: View {
           formType = .new(newImage)
         }
       }
+      .onChange(of: shareService.codeableImage) { codableImage in
+        if let codableImage {
+          if let myImage = myImages.first(where: {$0.id == codableImage.id}) {
+            // This is an upate of an existing image
+            updateImageInfo(myImage: myImage)
+            imageExists.toggle()
+          } else {
+            // This is a new MyImage Item
+            restoreMyImage()
+          }
+        }
+      }
       .sheet(item: $formType) { $0 }
+      .alert("Image Updated", isPresented: $imageExists) {
+        Button("OK") {}
+      }
     }
+  }
+
+  func restoreMyImage() {
+    if let codableImage = shareService.codeableImage {
+      let newImage = MyImage(context: moc)
+      newImage.name = codableImage.name
+      newImage.id = codableImage.id
+      newImage.comment = codableImage.comment
+      newImage.dateTaken = codableImage.dateTaken
+      newImage.receivedFrom = codableImage.receivedFrom
+      try? moc.save()
+    }
+    shareService.codeableImage = nil
+  }
+
+  func updateImageInfo(myImage: MyImage) {
+    if let codableImage = shareService.codeableImage {
+      myImage.name = codableImage.name
+      myImage.id = codableImage.id
+      myImage.comment = codableImage.comment
+      myImage.dateTaken = codableImage.dateTaken
+      myImage.receivedFrom = codableImage.receivedFrom
+      try? moc.save()
+    }
+    shareService.codeableImage = nil
   }
 }
 
--- iCloudCoreDataImageGallery/ShareService.swift
+++ iCloudCoreDataImageGallery/ShareService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import ZIPFoundation
 
 struct CodableImage: Codable, Equatable {
   let comment: String
@@ -16,15 +17,57 @@ struct CodableImage: Codable, Equatable {
 }
 
 class ShareService: ObservableObject {
+  @Published var codeableImage: CodableImage?
   static let ext = "myimg"
   func saveMyImage(_ codableImage: CodableImage) {
-    let filename = "\(codableImage.id).\(Self.ext)"
+    let filename = "\(codableImage.id).json"
     do {
       let data = try JSONEncoder().encode(codableImage)
       let jsonString = String(decoding: data, as: UTF8.self)
       FileManager().saveJSON(jsonString, fileName: filename)
+      zipFiles(id: codableImage.id)
     } catch {
       print("Could not encode data")
+    }
+  }
+  
+  func restore(url: URL) {
+    let fileName = url.lastPathComponent
+    let jsonName = fileName.replacingOccurrences(of: ShareService.ext, with: "json")
+    let zipName = fileName.replacingOccurrences(of: ShareService.ext, with: "zip")
+    let imgName = fileName.replacingOccurrences(of: ShareService.ext, with: "jpg")
+    let imgURL = URL.documentsDirectory.appending(path: imgName)
+    let zipURL = URL.documentsDirectory.appending(path: zipName)
+    let unzippedJSONURL = URL.documentsDirectory.appending(path: jsonName)
+    if url.pathExtension == Self.ext {
+      try? FileManager().moveItem(at: url, to: zipURL)
+      try? FileManager().removeItem(at: imgURL)
+      do {
+        try FileManager().unzipItem(at: zipURL, to: URL.documentsDirectory)
+      } catch {
+        print(error.localizedDescription)
+      }
+      if let codeableImage = FileManager().decodeJSON(from: URL.documentsDirectory.appending(path: jsonName)) {
+        self.codeableImage = codeableImage
+      }
+    }
+    try? FileManager().removeItem(at: zipURL)
+    try? FileManager().removeItem(at: unzippedJSONURL)
+  }
+  
+  func zipFiles(id: String) {
+    let archiveURL = URL.documentsDirectory.appending(path: "\(id).\(Self.ext)")
+    guard let archive = Archive(url: archiveURL, accessMode: .create) else {
+      return
+    }
+    let imageURL = URL.documentsDirectory.appending(path: "\(id).jpg")
+    let jsonURL = URL.documentsDirectory.appending(path: "\(id).json")
+    do {
+      try archive.addEntry(with: imageURL.lastPathComponent, relativeTo: URL.documentsDirectory)
+      try archive.addEntry(with: jsonURL.lastPathComponent, relativeTo: URL.documentsDirectory)
+      try FileManager().removeItem(at: jsonURL)
+    } catch {
+      print(error.localizedDescription)
     }
   }
 }
